b1ae378ffa7dbc73bc90029a8b9aecee517994b1
Pritom <98pritom@gmail.com>
2024-09-06 15:13:28 +0600

    movies-section-updated

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'MoviesHood',
       theme: theme,
       home: const HomeScreen(),
     );
